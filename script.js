0e56f5f44dd35a3fc7d5cb9da4b608d0114ddd1f
ryanorlando92 <ryan.orlando92@gmail.com>
2022-08-18 23:03:03 -0400

    add logic for playround

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function getCpuChoice() {
     }   else if (float > 33.33) {
         return 'PAPER';
     }   else {
-        return 'SCISSOR';
+        return 'SCISSORSgetPlayerChoice()getPlayerChoice()';
     };
 }
 
@@ -41,3 +41,39 @@ function getPlayerChoice() {
     };
 } 
 
+function playRound() {
+    let cpuSelection = getCpuChoice();
+    let playerSelection = getPlayerChoice();
+
+    if (cpuSelection === playerSelection) {
+        console.log( 'Tie!' );
+    
+    } else if (playerSelection === 'ROCK') {
+        // should you use a ternary operator here for succintness?
+        if (cpuSelection === 'PAPER') {
+            console.log( 'Paper beats rock! YOU LOSE' );
+
+        } else if (cpuSelection === 'SCISSORS') {
+            console.log( 'Rock beats scissors! YOU WIN' );
+
+        } else { console.log( 'somthing has gone wrong' ) };
+    
+    } else if (playerSelection === 'SCISSORS') {
+        if (cpuSelection === 'PAPER') {
+            console.log( 'Scissors beats paper! YOU WIN' );
+
+        } else if (cpuSelection === 'ROCK') {
+            console.log( 'Rock beats scissors! YOU LOSE' );
+
+        } else { console.log( 'something has gone wrong' ) };
+
+    } else if (playerSelection === 'PAPER') {
+        if (cpuSelection === 'SCISSORS') {
+            console.log( 'Scissors beats paper! YOU LOSE');
+
+        } else if (cpuSelection === 'ROCK') {
+            console.log( 'Paper beats rock! YOU WIN');
+        } else { console.log( 'something has gone wrong' ) };
+    } else { console.log( 'something has gone wrong' ) };
+    
+}
