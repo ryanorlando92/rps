c283c1f35de9d8989ee3b4ed01f910610132a4b2
ryanorlando92 <ryan.orlando92@gmail.com>
2022-08-18 22:03:35 -0400

    add function getPlayerChoice

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-let cpuChoice = '';
+let cpuChoice = null;
+let PlayerChoice = null;
 
 function getCpuChoice() {
 
     const float = Math.random() * 100;
 
     if (float > 66.67) {
-        return 'rock';
+        return 'ROCK';
 
     }   else if (float > 33.33) {
-        return 'paper';
+        return 'PAPER';
     }   else {
-        return 'scissor';
+        return 'SCISSOR';
     };
 }
+
+function getPlayerChoice() {
+    const userInput = prompt( 'Rock, Paper, or Scissors?' );
+    if ( userInput.toUpperCase() = 'ROCK' || 'PAPER' || 'SCISSOR' ) {
+        return userInput.toUpperCase();
+    } else {
+        alert( 'You have made an invalid selection' );
+    };
+} 
+
